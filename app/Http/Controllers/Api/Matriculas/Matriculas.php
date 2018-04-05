6c7b30d138d19596b05ec0dcf6bf1a23d5e535c4
MaTiUs77 <matius77@gmail.com>
2018-04-05 12:25:13 -0300

    Matriculas cuantitativas
    por division y por nivel de servicio con filtros

--- app/Http/Controllers/Api/Matriculas/Matriculas.php
+++ app/Http/Controllers/Api/Matriculas/Matriculas.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Api\Matriculas;
 
-use App\CursosInscripcions;
 use App\Http\Controllers\Controller;
 use App\Cursos;
-use App\Inscripcions;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
 
 class Matriculas extends Controller
 {
@@ -82,6 +83,176 @@ class Matriculas extends Controller
 
             return compact('reset','inscripciones');
         }
+    }
+    
+    public function cuantitativa(Request $request) {
+        // Reglas
+        $validationRules = [
+            'ciclo' => 'required|numeric',
+            'ciudad' => 'string',
+            'ciudad_id' => 'numeric',
+            'centro_id' => 'numeric',
+            'anio' => 'string',
+        ];
+
+        // Se validan los parametros
+        $validator = Validator::make($request->all(), $validationRules);
+        if ($validator->fails()) {
+            return ['error' => $validator->errors()];
+        }
+
+        $ciclo = Input::get('ciclo');
+        $ciudad = Input::get('ciudad');
+        $ciudad_id = Input::get('ciudad_id');
+        $centro_id = Input::get('centro_id');
+        $anio = Input::get('anio');
+
+        $filtros = [];
+
+        if(isset($ciclo)) {
+            $filtros[] = "ciclo.nombre = '$ciclo'";
+        }
+        if(isset($anio)) {
+            $filtros[] = "curso.anio = '$anio'";
+        }
+        if(isset($ciudad)) {
+            $filtros[] = "ciudad.nombre = '$ciudad'";
+        }
+        if(isset($ciudad_id)) {
+            $filtros[] = "ciudad.id = '$ciudad_id'";
+        }
+        if(isset($centro_id)) {
+            $filtros[] = "ins.centro_id = '$centro_id'";
+        }
+
+        $filtros = join(' AND ',$filtros);
+
+        $inscripciones = DB::select(
+            DB::raw(
+                "
+            select 
+                
+                ins.centro_id,
+                curso.id as curso_id,
+                ciudad.id as ciudad_id,
+
+                ciudad.nombre as ciudad,
+
+                centro.nombre,
+                
+                curso.anio,
+                curso.division,
+                curso.turno,
+                curso.plazas,
+                COUNT(ins.id) as matriculas,
+                (
+                  curso.plazas - COUNT(ins.id)
+                ) as vacantes
+                
+                FROM inscripcions ins
+                
+                inner join ciclos ciclo on ciclo.id = ins.ciclo_id
+                inner join centros centro on centro.id = ins.centro_id
+                inner join ciudads ciudad on ciudad.id = centro.ciudad_id
+                inner join cursos_inscripcions cui on cui.inscripcion_id = ins.id
+                inner join cursos curso on curso.id = cui.curso_id
+                
+                where
+                
+                (ins.estado_inscripcion = 'CONFIRMADA' or ins.estado_inscripcion = 'NO CONFIRMADA') AND
+                $filtros
+--              curso.division <> ''
+
+                group by 
+    
+                ins.centro_id,            
+                curso.id,
+                curso.anio,
+                curso.division,
+                curso.turno,
+                curso.plazas")
+        );
+
+        return $inscripciones;
+    }
+
+    public function cuantitativaPorNivel(Request $request) {
+        // Reglas
+        $validationRules = [
+            'ciclo' => 'required|numeric',
+            'ciudad' => 'string',
+            'ciudad_id' => 'numeric',
+            'centro_id' => 'numeric',
+            'nivel_servicio' => 'string',
+        ];
+
+        // Se validan los parametros
+        $validator = Validator::make($request->all(), $validationRules);
+        if ($validator->fails()) {
+            return ['error' => $validator->errors()];
+        }
+
+        $ciclo = Input::get('ciclo');
+        $ciudad = Input::get('ciudad');
+        $ciudad_id = Input::get('ciudad_id');
+        $centro_id = Input::get('centro_id');
+        $nivel_servicio = Input::get('nivel_servicio');
+
+
+        $filtros = [];
+
+        if(isset($ciclo)) {
+            $filtros[] = "ciclo.nombre = '$ciclo'";
+        }
+        if(isset($ciudad)) {
+            $filtros[] = "ciudad.nombre = '$ciudad'";
+        }
+        if(isset($ciudad_id)) {
+            $filtros[] = "ciudad.id = '$ciudad_id'";
+        }
+        if(isset($centro_id)) {
+            $filtros[] = "ins.centro_id = '$centro_id'";
+        }
+        if(isset($nivel_servicio)) {
+            $filtros[] = "centro.nivel_servicio= '$nivel_servicio'";
+        }
+        $filtros = join(' AND ',$filtros);
+
+        $inscripciones = DB::select(
+            DB::raw(
+                "
+            select 
+
+                ins.centro_id,
+                ciudad.id as ciudad_id,
+                ciudad.nombre as ciudad,
+                centro.nombre,
+                centro.nivel_servicio,
+                
+                COUNT(ins.id) as matriculas
+                
+            FROM inscripcions ins
+                
+            inner join ciclos ciclo on ciclo.id = ins.ciclo_id
+            inner join centros centro on centro.id = ins.centro_id
+            inner join ciudads ciudad on ciudad.id = centro.ciudad_id
+            inner join cursos_inscripcions cui on cui.inscripcion_id = ins.id
+            inner join cursos curso on curso.id = cui.curso_id
+                
+            where
+                
+                (ins.estado_inscripcion = 'CONFIRMADA' or ins.estado_inscripcion = 'NO CONFIRMADA') AND
+                $filtros
+
+            group by 
+                
+                ins.centro_id, 
+                ciudad.nombre,
+                centro.nivel_servicio
+            ")
+        );
+
+        return $inscripciones;
     }
 
     // Actualiza las vacantes con el valor de plazas, y las matriculas las deja en cero
--- app/Http/Controllers/Api/Matriculas/routes.php
+++ app/Http/Controllers/Api/Matriculas/routes.php
@@ -1,8 +1,14 @@
 <?php
 
-Route::prefix('matriculas')->middleware(['jwt.auth'])->group(function () {
-    Route::get('recuento/{cicloNombre}', 'Api\Matriculas\Matriculas@recuento');
-    Route::get('reset', 'Api\Matriculas\Matriculas@resetMatriculaYPlazas');
+Route::prefix('matriculas')->group(function () {
+    Route::get('cuantitativa/por_nivel', 'Api\Matriculas\Matriculas@cuantitativaPorNivel');
+    Route::get('cuantitativa', 'Api\Matriculas\Matriculas@cuantitativa');
+
+    Route::get('recuento/{cicloNombre}', 'Api\Matriculas\Matriculas@recuento')
+        ->middleware(['jwt.auth']);
+
+    Route::get('reset', 'Api\Matriculas\Matriculas@resetMatriculaYPlazas')
+        ->middleware(['jwt.auth']);
 });
 
 
