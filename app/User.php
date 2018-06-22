2322f17db62b59fd10e2be5eb05150c58de951b3
MaTiUs77 <matius77@gmail.com>
2018-06-22 10:13:12 -0300

    fix(Spatie/HasRole)

--- app/User.php
+++ app/User.php
@@ -4,11 +4,10 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable 
 {
-    use Notifiable, HasRoles;
+    use Notifiable;
 
     const CREATED_AT = 'created';
     const UPDATED_AT = 'modified';
