9949078da61c517fc86a129519f2038fc3d24380
MaTiUs77 <matius77@gmail.com>
2019-03-15 11:41:13 -0300

    Metodo (Export/resourceToExcel)
    Transforma un ResourceCollection para su exportacion a Excel

--- app/Http/Controllers/Api/Exportar/v1/Exportar.php
+++ app/Http/Controllers/Api/Exportar/v1/Exportar.php
@@ -6,26 +6,9 @@ use App\Http\Controllers\Api\Exportar\v1\Resources\ListaAlumnosResource;
 use App\Http\Controllers\Api\Utilities\ApiConsume;
 use App\Http\Controllers\Api\Utilities\Export;
 use App\Http\Controllers\Controller;
-use Prophecy\Exception\Doubler\MethodNotExtendableException;
 
 class Exportar extends Controller
 {
-    /*public function excel($render) {
-        $params = request()->all();
-
-        // Este metodo magico, permite acceder via URL al render solicitado
-        try {
-            return $this->$render($params);
-        } catch (\Exception $ex) {
-            if($ex instanceof MethodNotExtendableException)
-            {
-                return ['error' => 'Render no disponible'];
-            } else {
-                return ['error' => $ex->getMessage()];
-            }
-        }
-    }*/
-
     public function ListaAlumnos() {
         $params = request()->all();
         
@@ -39,22 +22,12 @@ class Exportar extends Controller
         $data = collect($response['data']);
         $sorted = $data->sortBy('inscripcion.alumno.persona.nombre_completo')->values();
 
-        // Transforma el resultado, con un formato pre-establecido
+        // Transforma el resultado, con un formato pre-establecido por el resource
         $alumnos = ListaAlumnosResource::collection(collect($sorted));
 
-        // Obtiene el nombre de las variables
-        $header = collect($alumnos->first())->keys()->toArray();
-
-        // Quita las variables y deja solo los valores
-        $excel = collect($alumnos)->map(function($i){
-           return collect($i)->flatten();
-        })->toArray();
-
-        // Se prepara para renderizar el excel
-        if(count($excel)>0)
+        if(count($alumnos)>0)
         {
-            array_unshift($excel,$header);
-            Export::toExcel('ListaAlumnos','Lista de Alumnos',$excel);
+            Export::resourceToExcel('ListaAlumnos','Lista de Alumnos',$alumnos);
         } else {
             return ['error' => 'No se obtuvieron resultados con el filtro aplicado'];
         }
