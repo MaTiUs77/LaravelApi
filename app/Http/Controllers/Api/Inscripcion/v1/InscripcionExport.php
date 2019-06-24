e67396e4c87043aa96ab87c6f2c85017beba8894
MaTiUs77 <matius77@gmail.com>
2019-06-24 10:37:18 -0300

    fix(Exportacion Excel Alumnos Nominales)
    Exporta y ordena correctamente los Alumnos por Secccion Nominal

--- app/Http/Controllers/Api/Inscripcion/v1/InscripcionExport.php
+++ app/Http/Controllers/Api/Inscripcion/v1/InscripcionExport.php
@@ -13,10 +13,7 @@ class InscripcionExport extends Controller
     public function excel()
     {
         $params = request()->all();
-
         // Consumo API Inscripciones
-        Log::debug("Iniciando exportacion excel",$params);
-
         $api = new ApiConsume();
         $api->get("inscripcion/lista",$params);
         if($api->hasError()) { return $api->getError(); }
@@ -25,11 +22,12 @@ class InscripcionExport extends Controller
         if($response!=null)
         {
             // Por defecto la lista se ordena por APELLIDOS y NOMBRES
-            $collection = collect($response->data);
+            $collection = collect($response['data']);
+
             $sorted = $collection->sortBy(function ($item, $key) {
                 // Requiere un saneo en la DB (alumnos sin personas)
-                if(isset($item->inscripcion->alumno->persona)) {
-                    return trim($item->inscripcion->alumno->persona->apellidos).",".$item->inscripcion->alumno->persona->nombres;
+                if(isset($item['inscripcion']['alumno']['persona'])) {
+                    return trim($item['inscripcion']['alumno']['persona']['apellidos']).",".$item['inscripcion']['alumno']['persona']['nombres'];
                 }
             })->values();
 
@@ -39,24 +37,31 @@ class InscripcionExport extends Controller
 
             // Contenido
             foreach($sorted as $index => $item) {
-                 $line = [
-                    $item->inscripcion->ciclo->nombre,
-                    $item->inscripcion->centro->nombre,
-                    $item->curso->anio,
-                    $item->curso->division,
-                    $item->curso->turno
+                $inscripcion = $item['inscripcion'];
+                $curso = $item['curso'];
+                $line = [
+                    $inscripcion['ciclo']['nombre'],
+                    $inscripcion['centro']['nombre'],
+                    $curso['anio'],
+                    $curso['division'],
+                    $curso['turno']
                 ];
 
-                if(isset($item->inscripcion->alumno->persona)){
-                    $line[] = $item->inscripcion->alumno->persona->documento_nro;
-                    $line[] = trim($item->inscripcion->alumno->persona->apellidos).",".title_case($item->inscripcion->alumno->persona->nombres);
+                if(isset($inscripcion['alumno']['persona'])){
+                    $persona = $inscripcion['alumno']['persona'];
+                    $line[] = $persona['documento_nro'];
+                    $line[] = trim($persona['apellidos']).",".title_case($persona['nombres']);
                 } else {
                     $line[] = '-';
                     $line[] = '-';
                 }
 
-                $line[] = $item->inscripcion->estado_inscripcion;
+                $line[] = $inscripcion['estado_inscripcion'];
                 $content[] = $line;
+
+                $inscripcion = null;
+                $curso  = null;
+                $persona = null;
             }
 
             Export::toExcel('Inscripciones','Lista',$content);
