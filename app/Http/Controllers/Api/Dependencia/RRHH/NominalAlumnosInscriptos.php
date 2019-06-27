045fbda8c790e8aec87cd17a6bfb2eb1eadf8980
MaTiUs77 <matius77@gmail.com>
2019-06-27 13:24:30 -0300

    Consumo a travez de ApiConsume
    Optimizacion de consumo de datos

--- app/Http/Controllers/Api/Dependencia/RRHH/NominalAlumnosInscriptos.php
+++ app/Http/Controllers/Api/Dependencia/RRHH/NominalAlumnosInscriptos.php
@@ -1,9 +1,9 @@
 <?php
 namespace App\Http\Controllers\Api\Dependencia\RRHH;
 
+use App\Http\Controllers\Api\Utilities\ApiConsume;
 use App\Http\Controllers\Api\Utilities\Export;
 use App\Http\Controllers\Controller;
-use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Input;
 
 class NominalAlumnosInscriptos extends Controller
@@ -15,67 +15,59 @@ class NominalAlumnosInscriptos extends Controller
 
     public function start()
     {
-        // Consume API lista de inscripciones
-        $guzzle = new Client();
-        $consumeApi = $guzzle->get(env('SIEP_LARAVEL_API')."/api/inscripcion/lista",[
-            'query' => Input::all()
-        ]);
+        // Por defecto API Consume ejecuta la ruta /api/v1/{path}
+        $params = Input::all();
+        $api = new ApiConsume();
+        $api->get("inscripcion/lista",$params);
+        if($api->hasError()) { return $api->getError(); }
+        $response= $api->response();
 
-        // Obtiene el contenido de la respuesta, la transforma a json
-        $content = $consumeApi->getBody()->getContents();
-        $lista = json_decode($content,true);
+        // Transforma los datos a collection para realizar un mapeo
+        $data = collect($response['data']);
 
-        // Si no esta definido el error, procedemos a formatear los datos
-        if(!isset($lista['error']))
-        {
-            // Transforma los datos a collection para realizar un mapeo
-            $data = collect($lista['data']);
+        // Optimizar:: Usar Resource en vez de un mapeo por array
+        $formatted = $data->map(function($item){
+            $inscripcion = $item['inscripcion'];
+            $curso = $item['curso'];
 
-            $formatted = $data->map(function($item){
-                $inscripcion = $item['inscripcion'];
-                $curso = $item['curso'];
+            $ciclo = $inscripcion['ciclo'];
+            $centro = $inscripcion['centro'];
+            $persona = $inscripcion['alumno']['persona'];
 
-                $ciclo = $inscripcion['ciclo'];
-                $centro = $inscripcion['centro'];
-                $persona = $inscripcion['alumno']['persona'];
+            return [
+                'documento_tipo' => $persona['documento_tipo'],
+                'inscripcion_id' => $inscripcion['id'],
+                'dni' => $persona['documento_nro'],
+                'nombres' => $persona['nombres'],
+                'apellidos' => $persona['apellidos'],
+                'nombre_completo' => $persona['nombre_completo'],
+                'sexo' => $persona['sexo'],
+                'edad' => $persona['edad'],
+                'ciclo' => $ciclo['nombre'],
+                'centro' => $centro['nombre'],
+                'nivel_servicio' => $centro['nivel_servicio'],
+                'año' => $curso['anio'],
+                'division' => $curso['division'],
+                'turno' => $curso['turno'],
+                'fecha_alta' => $inscripcion['fecha_alta'],
+                'fecha_baja' => $inscripcion['fecha_baja'],
+                'fecha_egreso' => $inscripcion['fecha_egreso'],
+                'calle_nombre' =>$persona['calle_nombre'],
+                'calle_nro' =>$persona['calle_nro'],
+                'pcia_nac' =>$persona['pcia_nac'],
+                'email' =>$persona['email'],
+                'telefono_nro' =>$persona['telefono_nro'],
+                'observaciones' =>$persona['observaciones'],
+                'fecha_nac' => $persona['fecha_nac']
+            ];
+        });
 
-                return [
-                    'documento_tipo' => $persona['documento_tipo'],
-                    'inscripcion_id' => $inscripcion['id'],
-                    'dni' => $persona['documento_nro'],
-                    'nombres' => $persona['nombres'],
-                    'apellidos' => $persona['apellidos'],
-                    'nombre_completo' => $persona['nombre_completo'],
-                    'sexo' => $persona['sexo'],
-                    'edad' => $persona['edad'],
-                    'ciclo' => $ciclo['nombre'],
-                    'centro' => $centro['nombre'],
-                    'nivel_servicio' => $centro['nivel_servicio'],
-                    'año' => $curso['anio'],
-                    'division' => $curso['division'],
-                    'turno' => $curso['turno'],
-                    'fecha_alta' => $inscripcion['fecha_alta'],
-                    'fecha_baja' => $inscripcion['fecha_baja'],
-                    'fecha_egreso' => $inscripcion['fecha_egreso'],
-                    'calle_nombre' =>$persona['calle_nombre'],
-                    'calle_nro' =>$persona['calle_nro'],
-                    'pcia_nac' =>$persona['pcia_nac'],
-                    'email' =>$persona['email'],
-                    'telefono_nro' =>$persona['telefono_nro'],
-                    'observaciones' =>$persona['observaciones'],
-                    'fecha_nac' => $persona['fecha_nac']
-                ];
-            });
+        $response['data'] = $formatted;
 
-            $lista['data'] = $formatted;
+        // Exportacion a Excel si es solicitado
+        $this->exportar($formatted);
 
-            // Exportacion a Excel si es solicitado
-            $this->exportar($formatted);
-
-            return $lista;
-        }
-
-        return $lista;
+        return $response;
     }
 
     private function exportar($lista) {
