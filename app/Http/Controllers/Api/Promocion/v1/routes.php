e07ce693ec42d3d4635698a21a647cc7ccaa0539
MaTiUs77 <matius77@gmail.com>
2019-08-13 13:51:24 -0300

    Promocion y Repitencia Optimizadas
    Actualiza nuevos campos, requiere ciclo

--- app/Http/Controllers/Api/Promocion/v1/routes.php
+++ app/Http/Controllers/Api/Promocion/v1/routes.php
@@ -4,6 +4,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('promocion')->group(function () {
 
         Route::get('/', 'Api\Promocion\v1\PromocionCrud@index');
+        Route::post('/', 'Api\Promocion\v1\PromocionCrud@store');
 
         Route::get('{ciclo}', 'Api\Promocion\v1\PromocionRouteFilter@index');
         Route::get('{ciclo}/{centro_id}', 'Api\Promocion\v1\PromocionRouteFilter@index');
