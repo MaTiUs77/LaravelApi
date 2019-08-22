034b9ee945048177e3dede519b73c11677f684cb
MaTiUs77 <matius77@gmail.com>
2019-08-22 13:48:22 -0300

    Trayectoria de personas
    Pendiente optimizacion en laravel 5.8

--- app/Http/Controllers/Api/Personas/v1/PersonaUltimaInscripcion.php
+++ app/Http/Controllers/Api/Personas/v1/PersonaUltimaInscripcion.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Personas\v1;
 
+use App\CursosInscripcions;
 use App\Http\Controllers\Api\Utilities\ApiConsume;
 use App\Http\Controllers\Controller;
 
@@ -11,11 +12,12 @@ class PersonaUltimaInscripcion extends Controller
 {
     public function index(Personas $persona)
     {
+        $params = [
+            'with' => 'alumnos'
+        ];
         // Consumo API Inscripciones
         $apiPersona= new ApiConsume();
-        $apiPersona->get("personas/{$persona->id}",[
-            'render' => 'trayectoria'
-        ]);
+        $apiPersona->get("personas/{$persona->id}",$params);
 
         if($apiPersona->hasError()) { return $apiPersona->getError(); }
 
