de2794c6fd1372e9643f112a69f952d07c4ea627
MaTiUs77 <matius77@gmail.com>
2019-08-22 13:43:13 -0300

    Normalizacion de rutas de matriculas
    Se mueve el archivo rutas a su respectiva carpeta v1

--- app/Http/Controllers/Api/Matriculas/routes.php
+++ app/Http/Controllers/Api/Matriculas/routes.php
@@ -14,15 +14,3 @@ Route::prefix('matriculas')->group(function () {
     Route::get('reset', 'Api\Matriculas\Matriculas@resetMatriculaYPlazas');
 
 });
-
-
-Route::prefix('v1')->group(function () {
-    Route::prefix('matriculas')->group(function () {
-        Route::prefix('cuantitativa')->group(function () {
-            Route::get('por_seccion', 'Api\Matriculas\v1\MatriculasPorSeccion@start');
-            Route::get('por_establecimiento', 'Api\Matriculas\v1\MatriculasPorEstablecimiento@start');
-            Route::get('por_anio', 'Api\Matriculas\v1\MatriculasPorAnio@start');
-            Route::get('por_nivel', 'Api\Matriculas\v1\MatriculasPorNivel@start');
-        });
-    });
-});
