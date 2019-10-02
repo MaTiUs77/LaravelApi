d3bd9eda723388a98837d744da36a34400e2edda
MaTiUs77 <matius77@gmail.com>
2019-10-02 17:18:41 -0300

    Pase Legacy
    Deprecar luego del saneo

--- app/Inscripcions.php
+++ app/Inscripcions.php
@@ -69,9 +69,19 @@ class Inscripcions extends Model
         );
     }
 
+    // Deprecar logica de relacion
     function Origen()
     {
-        //return $this->belongsTo('App\Inscripcions', 'repitencia_id', 'id')->with(['curso','centro']);
+        return $this->hasOne('App\Centros', 'id', 'centro_origen_id');
+    }
+
+    // Deprecar logica de relacion
+    function Pase()
+    {
+        // Relacion correcta luego del saneo
+        // return $this->belongsTo('App\Inscripcions', 'pase_id', 'id')->with(['curso','centro']);
+
+        // Deprecar relacion con columna actual
         return $this->hasOne('App\Centros', 'id', 'centro_origen_id');
     }
 }
